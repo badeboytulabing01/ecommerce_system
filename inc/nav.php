63433f1814eaa63ac8512e46011379e7e4b8fb73
Dashotz <d.shotz@yahoo.com>
2024-05-26 14:38:14 +0800

    nav
    changed the login link

--- inc/nav.php
+++ inc/nav.php
@@ -3,7 +3,7 @@
         <div class="nav-links">
           <ul>            
           <i class="fas fa-shopping-cart"></i> <li><a href="addtocart.php">Cart</a></li>
-          <i class="fa-regular fa-user"></i><li><a href="login_form.php">Login/Sign-up</a></li>
+          <i class="fa-regular fa-user"></i><li><a href="login.php">Login/Sign-up</a></li>
 
         </div>
       </nav>
